78d3fd439e58e997888e85520eab26227833d04d
kne17 <kne2317@tukorea.ac.kr>
2025-10-25 11:40:40 +0900

    Initial commit

---
+++ src/main/java/com/example/trip_maker_server/TripMakerServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.trip_maker_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripMakerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripMakerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trip_maker_server/TripMakerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trip_maker_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripMakerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
